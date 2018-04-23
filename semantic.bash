c2b38b1637654bee5519e4378f1e043a2da98ea6
Little Round <lxy9843@qq.com>
2018-04-23 18:12:16 +0800

    Debug..

--- semantic.bash
+++ semantic.bash
@@ -7,4 +7,4 @@ set -e
 cd "$(dirname "$0")"
 export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin cn.littleround.Main"
 cat > program.txt   # save everything in stdin to program.txt
-$CCHK
+$CCHK @/dev/stdin
