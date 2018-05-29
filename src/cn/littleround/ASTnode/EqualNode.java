beb5d9397701a5383f7325ce6570f3aa261c3a5f
Little Round <lxy9843@qq.com>
2018-05-30 04:11:07 +0800

    unfinished version   - bug fix: also should use mask in equal/inequalNode

--- src/cn/littleround/ASTnode/EqualNode.java
+++ src/cn/littleround/ASTnode/EqualNode.java
@@ -3,9 +3,11 @@ package cn.littleround.ASTnode;
 import cn.littleround.Constants;
 import cn.littleround.ir.Function;
 import cn.littleround.nasm.BasicBlock;
+import cn.littleround.nasm.Instruction.AndLine;
 import cn.littleround.nasm.Instruction.CmpLine;
 import cn.littleround.nasm.Instruction.SetELine;
 import cn.littleround.nasm.Instruction.SetLELine;
+import cn.littleround.nasm.Operand.DecimalOperand;
 import cn.littleround.nasm.Operand.VirtualRegOperand;
 import cn.littleround.type.BoolType;
 
@@ -34,6 +36,7 @@ public class EqualNode extends BinaryOpNode {
         ));
         //bb.add(new XorLine(new VirtualRegOperand(vid))); // xor change flags!!!
         bb.add(new SetELine(new VirtualRegOperand(vid)));
+        bb.add(new AndLine(new VirtualRegOperand(vid), new DecimalOperand(Constants.byteMask)));
         f.nctx().setNodeVid(this ,vid);
         BasicBlock.dequeCombine(ret, bb);
         return ret;
--- src/cn/littleround/ASTnode/InequalNode.java
+++ src/cn/littleround/ASTnode/InequalNode.java
@@ -3,9 +3,11 @@ package cn.littleround.ASTnode;
 import cn.littleround.Constants;
 import cn.littleround.ir.Function;
 import cn.littleround.nasm.BasicBlock;
+import cn.littleround.nasm.Instruction.AndLine;
 import cn.littleround.nasm.Instruction.CmpLine;
 import cn.littleround.nasm.Instruction.SetELine;
 import cn.littleround.nasm.Instruction.SetNELine;
+import cn.littleround.nasm.Operand.DecimalOperand;
 import cn.littleround.nasm.Operand.VirtualRegOperand;
 
 import java.util.ArrayDeque;
@@ -33,6 +35,7 @@ public class InequalNode extends BinaryOpNode {
         ));
         //bb.add(new XorLine(new VirtualRegOperand(vid))); // xor change flags!!!
         bb.add(new SetNELine(new VirtualRegOperand(vid)));
+        bb.add(new AndLine(new VirtualRegOperand(vid), new DecimalOperand(Constants.byteMask)));
         f.nctx().setNodeVid(this ,vid);
         BasicBlock.dequeCombine(ret, bb);
         return ret;
